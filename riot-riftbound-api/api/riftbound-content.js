add043e6e959e8a547d9445e69a8b2e8dec07a88
Connor Lafferty <gummyoshi@gmail.com>
2026-06-03 22:01:17 -0400

    new origins

--- riot-riftbound-api/api/riftbound-content.js
+++ riot-riftbound-api/api/riftbound-content.js
@@ -5,6 +5,7 @@ const CACHE_TIME_MS = 1000 * 60 * 15;
 export default async function handler(req, res) {
   const allowedOrigins = [
     "http://127.0.0.1:5500",
+    "http://localhost:5500",
     "https://gummyosh1.github.io"
   ];
 
